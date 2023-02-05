0476465c2176dbb4d764a4cd76995cd836b4994d
Strakatov Nikolay <desux327@gmail.com>
2023-02-05 08:37:57 +0600

    New

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,52 +1,36 @@
 ﻿using System.Numerics;
 
-
-BigInteger number = 2;
-
-Console.WriteLine("Введите целое число (степень), в которую хотите возвести число 2");
+Console.WriteLine("Введите целое число (степень) чтобы возвести в нее число 2");
 string input = Console.ReadLine() ?? "";
 
-if (!string.IsNullOrEmpty(input))
+if (!string.IsNullOrEmpty(input) && int.TryParse(input, out int power))
 {
-
-    _ = int.TryParse(input, out int power);
-
     if (power == 0)
     {
-        Console.WriteLine("Вы ввели 0 или неподходящие символы, попробуйте снова");
-        return;
+        Console.WriteLine("Число 2 нельзя возводить в степень 0, попробуйте снова");
     }
-    if (power == 1)
+    else if (power == 1)
     {
-        Console.WriteLine("Вы ввели степень 1, число не изменилось. Все также 2. Попробуйте снова");
-        return;
+        Console.WriteLine("Число 2 возведенное в степень 1 это все также 2");
     }
-    if (power < 0)
+    else if (power < 0)
     {
-        Console.WriteLine("Вы ввели отрицательную степень, попробуйте снова");
+        Console.WriteLine("Отрицательные степени не поддерживаются, попробуйте снова");
     }
-
-    if (power <= 999)
+    else if (power > 999)
     {
-        int count = 0;
-        for (int i = 0; i < power; i++)
-        {
-            number *= 2;
-            count++;
-
-            Console.Write($"Вы возвели 2 в степень {count} || Вы получили число {number}\n");
-        }
+        Console.WriteLine("Число которые вы ввели больше чем 999, попробуйте снова");
     }
     else
     {
-        Console.WriteLine("Вы ввели число 1000 или больше, попробуйте снова");
+        BigInteger result = BigInteger.Pow(2, power);
+        Console.WriteLine($"Результат возведения числа 2 в степень {power} это {result}");
     }
 }
 else
 {
-    Console.WriteLine("Вы отправили пустую строку, попробуйте снова");
+    Console.WriteLine("Неправильный ввод, попробуйте снова");
 }
-
 
 
 
